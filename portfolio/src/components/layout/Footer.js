82e89c5382d59fcd07e8632bee4b9f4bf9dc637c
Martin <jjooga@Martins-MacBook-Pro.local>
2025-09-28 00:28:49 +0200

    deployment files

--- portfolio/src/components/layout/Footer.js
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="py-6 text-center text-gray-600 border-t border-gray-200 font-mono">
-      © {new Date().getFullYear()} All rights reserved.
-    </footer>
-  );
-}
